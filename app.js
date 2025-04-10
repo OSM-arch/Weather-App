e8e60d7d91096d0962649cdc466291feeaf66953
OSM-arch <elmohtadioussama03@gmail.com>
2025-04-10 12:32:17 +0100

    feat: better date & error handling

--- app.js
+++ app.js
@@ -17,33 +17,13 @@ function setDate() {
 }
 
 function weekDay(day) {
-    switch (day) {
-        case 0: return 'Monday';
-        case 1: return 'Tuesday';
-        case 2: return 'Wednesday';
-        case 3: return 'Thursday';
-        case 4: return 'Friday';
-        case 5: return 'Saturday ';
-        case 6: return 'Sunday';
-    }
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    return weekday[day];
 }
 
 function monthDay(month) {
-    switch (month) {
-        case 0: return "January";
-        case 1: return "February";
-        case 2: return "March";
-        case 3: return "April";
-        case 4: return "May";
-        case 5: return "June";
-        case 6: return "July";
-        case 7: return "August";
-        case 8: return "September";
-        case 9: return "October";
-        case 10: return "November";
-        case 11: return "December";
-    }
-
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return months[month];
 }
 
 
