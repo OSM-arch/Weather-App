c377429ade95242ca80f78e6ae98fe769916b0d2
OSM-arch <elmohtadioussama03@gmail.com>
2025-08-19 01:20:50 +0100

    vite configuration base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Weather_App/",
+  base: "/Weather_App",
 })
